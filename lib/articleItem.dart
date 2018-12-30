5028b949ff47d893227884acbe6f87bf951adcd0
Andrey Golubkow <Andreygolubkow@gmailmail.com>
2018-12-30 20:20:45 +0700

    Переформатировал код. Поправил запись с aspectRatio в плеере.

--- lib/articleItem.dart
+++ lib/articleItem.dart
@@ -9,28 +9,23 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutube/flutube.dart';
 
 class ArticleItem extends StatelessWidget {
-  ArticleItem({ Key key, @required this.article, this.shape })
+  ArticleItem({Key key, @required this.article, this.shape})
       : assert(article != null),
         super(key: key);
 
   final Article article;
   final ShapeBorder shape;
   final DateFormat dateFormat = new DateFormat("HH:mm dd.MM.yyyy");
-  final RegExp iframeVideoRegExp = new RegExp("<iframe[^>]* src=\"([\^\"]*)\"[^>]*>");
-  final RegExp youTubeRegExp = new RegExp("((?:https?:\\/\\/)?(?:www\\.)?youtu\\.?be(?:\.com)?\\/?.*(?:watch|embed)?(?:.*v=|v\\/|\\/)([\\w\\-_]+)\\&?)\"");
+  final RegExp iframeVideoRegExp =
+      new RegExp("<iframe[^>]* src=\"([\^\"]*)\"[^>]*>");
+  final RegExp youTubeRegExp = new RegExp(
+      "((?:https?:\\/\\/)?(?:www\\.)?youtu\\.?be(?:\.com)?\\/?.*(?:watch|embed)?(?:.*v=|v\\/|\\/)([\\w\\-_]+)\\&?)\"");
   final RegExp imageRegExp = new RegExp("<img[^>]* src=\"([\^\"]*)\"[^>]*>");
-
 
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
-      top: false,
-      bottom: false,
-      child: Center(
-        child: BuildCard(context)
-      )
-    );
+        top: false, bottom: false, child: Center(child: BuildCard(context)));
   }
 
   _launchURL(String url) async {
@@ -41,49 +36,49 @@ class ArticleItem extends StatelessWidget {
     }
   }
 
-  bool IsContainImage(String text)
-  {
+  bool IsContainImage(String text) {
     return imageRegExp.hasMatch(text);
   }
 
-  String GetFirstImage(String text)
-  {
+  String GetFirstImage(String text) {
     return imageRegExp.firstMatch(text).group(1);
   }
 
-  bool IsContainYouTube(String text)
-  {
+  bool IsContainYouTube(String text) {
     return youTubeRegExp.hasMatch(article.text);
   }
 
-  String GetYouTubeLink(String text)
-  {
+  String GetYouTubeLink(String text) {
     return "https://www.youtube.com/watch?v=${youTubeRegExp.firstMatch(article.text).group(2)}";
   }
 
-  BuildTextCard(BuildContext context)
-  {
-    final articleTextNorm = article.text.replaceAll("//sports.kz", "http://sports.kz");
+  BuildTextCard(BuildContext context) {
+    final articleTextNorm =
+        article.text.replaceAll("//sports.kz", "http://sports.kz");
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ListTile(
             leading: ClipRRect(
-              child: IsContainImage(articleTextNorm) ? new CachedNetworkImage(
-                imageUrl: GetFirstImage(articleTextNorm),
-                placeholder: new CircularProgressIndicator(),
-                errorWidget: new Icon(Icons.error),
-                fit: BoxFit.cover,
-                width: 70.0,
-                height: 70.0,
-              ) : new Icon(Icons.photo),
+              child: IsContainImage(articleTextNorm)
+                  ? new CachedNetworkImage(
+                      imageUrl: GetFirstImage(articleTextNorm),
+                      placeholder: new CircularProgressIndicator(),
+                      errorWidget: new Icon(Icons.error),
+                      fit: BoxFit.cover,
+                      width: 70.0,
+                      height: 70.0,
+                    )
+                  : new Icon(Icons.photo),
               borderRadius: BorderRadius.circular(10),
             ),
             title: Text(article.title),
-            subtitle: Text('${dateFormat.format(article.lastModifyDateTime.toLocal())}'),
+            subtitle: Text(
+                '${dateFormat.format(article.lastModifyDateTime.toLocal())}'),
           ),
-          ButtonTheme.bar( // make buttons use the appropriate styles for cards
+          ButtonTheme.bar(
+            // make buttons use the appropriate styles for cards
             child: ButtonBar(
               children: <Widget>[
                 FlatButton(
@@ -92,7 +87,8 @@ class ArticleItem extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ViewArticleTextScreen(article: article),
+                        builder: (context) =>
+                            ViewArticleTextScreen(article: article),
                       ),
                     );
                   },
@@ -111,24 +107,22 @@ class ArticleItem extends StatelessWidget {
     );
   }
 
-  BuildVideoCard(BuildContext context)
-  {
-    final articleTextNorm = article.text.replaceAll("//sports.kz", "http://sports.kz");
+  BuildVideoCard(BuildContext context) {
+    final articleTextNorm =
+        article.text.replaceAll("//sports.kz", "http://sports.kz");
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-              FluTube(
-                  GetYouTubeLink(articleTextNorm),
-                  autoInitialize: true,
-                  looping: true,
-                  aspectRatio: 16 / 9
-                ),
+          FluTube(GetYouTubeLink(articleTextNorm),
+              autoInitialize: true, looping: true,aspectRatio: (16 / 9),),
           ListTile(
             title: Text(article.title),
-            subtitle: Text('${dateFormat.format(article.lastModifyDateTime.toLocal())}'),
+            subtitle: Text(
+                '${dateFormat.format(article.lastModifyDateTime.toLocal())}'),
           ),
-          ButtonTheme.bar( // make buttons use the appropriate styles for cards
+          ButtonTheme.bar(
+            // make buttons use the appropriate styles for cards
             child: ButtonBar(
               children: <Widget>[
                 FlatButton(
@@ -137,7 +131,8 @@ class ArticleItem extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ViewArticleTextScreen(article: article),
+                        builder: (context) =>
+                            ViewArticleTextScreen(article: article),
                       ),
                     );
                   },
@@ -150,15 +145,11 @@ class ArticleItem extends StatelessWidget {
     );
   }
 
-  Card BuildCard(BuildContext context)
-  {
-    if (IsContainYouTube(article.text))
-    {
+  Card BuildCard(BuildContext context) {
+    if (IsContainYouTube(article.text)) {
       return BuildVideoCard(context);
     } else {
       return BuildTextCard(context);
     }
   }
 }
-
-
